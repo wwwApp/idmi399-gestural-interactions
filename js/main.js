f43076074695b755202f20d9d0357cfb2bc31eb0
Woo Song <ws392@drexel.edu>
2019-10-26 01:03:43 -0400

    translate morse and implement tts api

--- js/main.js
+++ js/main.js
@@ -1,17 +1,58 @@
-// Setup Leap loop with frame callback function
-var controllerOptions = { enableGestures: true };
+const morseGuide = {
+  ".-": "a",
+  "-...": "b",
+  "-.-.": "c",
+  "-..": "d",
+  ".": "e",
+  "..-.": "f",
+  "--.": "g",
+  "....": "h",
+  "..": "i",
+  ".---": "j",
+  "-.-": "k",
+  ".-..": "l",
+  "--": "m",
+  "-.": "n",
+  "---": "o",
+  ".--.": "p",
+  "--.-": "q",
+  ".-.": "r",
+  "...": "s",
+  "-": "t",
+  "..-": "u",
+  "...-": "v",
+  ".--": "w",
+  "-..-": "x",
+  "-.--": "y",
+  "--..": "z",
+  ".----": "1",
+  "..---": "2",
+  "...--": "3",
+  "....-": "4",
+  ".....": "5",
+  "-....": "6",
+  "--...": "7",
+  "---..": "8",
+  "----.": "9",
+  "-----": "0"
+};
 
-// to use HMD mode:
-// controllerOptions.optimizeHMD = true;
+// Setup Leap loop with frame callback function
+var controllerOptions = { enableGestures: false };
 
 // Store morse code peice (dot or dash) to be printed
 var handIn = false;
-var morse = "";
+var signal = ""; // such as - . /
+var morse = ""; // morse code for each letter/number
+var translation = ""; // complete translation of input
 
 // Store the duration of last hand in
 // for translation/rendering
 var lastHandIn = 0;
 
+/**
+ * Leap Loop to process data
+ */
 Leap.loop(controllerOptions, function(frame) {
   if (frame.hands.length > 0) {
     var dataString = "";
@@ -22,46 +63,74 @@ Leap.loop(controllerOptions, function(frame) {
     // recongize dash and dot based on duration
     for (var i = 0; i < frame.hands.length; i++) {
       var hand = frame.hands[i];
-      // console.log(hand.timeVisible);
 
       // read the pattern
       if (hand.timeVisible > 1 && hand.grabStrength > 0.5) {
-        morse = "-";
+        signal = "-";
         $(".legend__status").removeClass("active");
         $(".legend--dash .legend__status").addClass("active");
       } else if (hand.timeVisible < 1 && hand.grabStrength > 0.5) {
-        morse = ".";
+        signal = ".";
         $(".legend__status").removeClass("active");
         $(".legend--dot .legend__status").addClass("active");
       } else if (hand.timeVisible > 1 && hand.grabStrength < 0.5) {
         // separate word
-        morse = "/";
+        signal = "/";
         $(".legend__status").removeClass("active");
         $(".legend--word .legend__status").addClass("active");
       } else if (hand.timeVisible < 1 && hand.grabStrength < 0.5) {
         // separate letter
-        morse = " ";
+        signal = " ";
         $(".legend__status").removeClass("active");
         $(".legend--letter .legend__status").addClass("active");
       }
     }
-
-    // update data value displayed in the interface
-    // dataString +=
-    //   "Hand Grabbed: <span class='data-partial'>" + hand.grabStrength > 0
-    //     ? "TRUE"
-    //     : "FALSE" + "</span><br />";
-    // dataString +=
-    //   "Hand Visible Time: <span class='data-partial'>" +
-    //   hand.timeVisible +
-    //   "</span>";
   } else if (frame.hand.length > 0 && handIn) {
     // print your code when your hand is in AND out
     // and make sure toggle handIn to false for next input
-    $(".code-reader__display").append(morse);
+    $(".code-reader__display").append(signal);
+    morse += signal;
+
+    if (signal === "/" || signal === " ") {
+      let translatedMorse = translate(morse);
+      $(".code-translator__display").append(translatedMorse);
+
+      translation += translatedMorse;
+    }
+
     handIn = false;
   } else {
     $(".code-reader__status").removeClass("active");
     $(".legend__status").removeClass("active");
   }
+});
+
+/**
+ * Function to translate morse code
+ */
+function translate(morseCode) {
+  // reset current morse piece for next letter
+  morse = "";
+
+  // if input is '/', simply add space to separate translated words
+  // otherwise, translate
+  if (morseCode != "/") {
+    // make sure to remove ' ' at the end for translation
+    let processedMorseCode = morseCode.slice(0, -1);
+    return morseGuide[processedMorseCode];
+  } else {
+    return " ";
+  }
+}
+
+$("#readButton").on("click", function() {
+  VoiceRSS.speech({
+    key: "b712c895b8fa4d2baedbdca044d05f6c",
+    src: translation,
+    hl: "en-us",
+    r: 0,
+    c: "mp3",
+    f: "44khz_16bit_stereo",
+    ssml: false
+  });
 });
